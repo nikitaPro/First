6c5d6b905b99952b91abe7da76e9f230215de8e2
nikitaPro <nikita_prokopenko@yahoo.com>
2017-09-28 13:54:22 +0300

    sort ready

--- src/com/netcracker/Main.java
+++ src/com/netcracker/Main.java
@@ -3,7 +3,7 @@ package com.netcracker;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sort(args);
         print(args);
     }
     public static void print(String[] args){
@@ -11,5 +11,32 @@ public class Main {
             System.out.println(args[i]);
         }
     }
-
+    public static void sort(String[] args){
+        if(args.length==0)return;
+        String buf;
+        String min;
+        int k=0;
+        int minIndex;
+        boolean needExchange;
+        do {
+            needExchange=false;
+            min=args[k];
+            minIndex=k;
+            for (int j = k+1; j <args.length ; j++) {
+                if(min.compareTo(args[j])>0){
+                    min= args[j];
+                    minIndex=j;
+                }
+                if(!needExchange && args[j-1].compareTo(args[j])>0)
+                    needExchange=true;
+            }
+            if(minIndex!=k) {
+                buf = args[minIndex];
+                args[minIndex] = args[k];
+                args[k] = buf;
+            }
+            k++;
+            if(k==args.length)return;
+        }while(needExchange);
+    }
 }
