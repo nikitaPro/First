c84e003403b6b917bc49fd845c8b892975487120
nikitaPro <nikita_prokopenko@yahoo.com>
2017-09-28 12:58:35 +0300

    print ready

--- src/com/netcracker/Main.java
+++ src/com/netcracker/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        print(args);
     }
+    public static void print(String[] args){
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+
 }
